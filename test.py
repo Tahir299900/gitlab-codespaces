9b1b0629225a6d368ac0768cbf301fe192c7b9cf
Tahir299900 <tahir999992@gmail.com>
2025-09-04 11:29:58 +0000

    Update GitLab CI config, test script, and add requirements.txt

--- test.py
+++ test.py
@@ -1,1 +1,29 @@
-print('hello gitlab ci')
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+LT_USERNAME = os.environ.get("LT_USERNAME")
+LT_ACCESS_KEY = os.environ.get("LT_ACCESS_KEY")
+
+if not (LT_USERNAME and LT_ACCESS_KEY):
+    raise SystemExit("Please set LT_USERNAME and LT_ACCESS_KEY environment variables")
+
+hub_url = f"https://{LT_USERNAME}:{LT_ACCESS_KEY}@hub.lambdatest.com/wd/hub"
+
+capabilities = {
+    "browserName": "chrome",
+    "browserVersion": "latest",
+    "platform": "Windows 10",
+    "build": "GitLab-CI",
+    "name": "simple-test"
+}
+
+chrome_options = Options()
+for key, value in capabilities.items():
+    chrome_options.set_capability(key, value)
+
+driver = webdriver.Remote(command_executor=hub_url, options=chrome_options)
+
+driver.get("https://example.com")
+print("Title:", driver.title)
+driver.quit()
